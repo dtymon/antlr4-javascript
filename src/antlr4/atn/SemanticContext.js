bc8840a7df10d2eb00e13415597602b4f7c7872e
David Tymon <david.tymon@gmail.com>
2015-09-03 10:14:51 +1000

    Fix bugs in SemanticContext
    Copy-and-paste error with AND.toString() function being reused for
    the OR.toString() function.
    
    Also, change "SemanticPredicate" to SemanticContext

--- src/antlr4/atn/SemanticContext.js
+++ src/antlr4/atn/SemanticContext.js
@@ -301,7 +301,7 @@ AND.prototype.evalPrecedence = function(parser, outerContext) {
 	}
 	var result = null;
 	operands.map(function(o) {
-		result = result === null ? o : SemanticPredicate.andContext(result, o);
+		result = result === null ? o : SemanticContext.andContext(result, o);
 	});
 	return result;
 };
@@ -363,7 +363,7 @@ OR.prototype.constructor = function(other) {
 };
 
 OR.prototype.hashString = function() {
-	return "" + this.opnds + "/OR"; 
+	return "" + this.opnds + "/OR";
 };
 
 // <p>
@@ -408,7 +408,7 @@ OR.prototype.evalPrecedence = function(parser, outerContext) {
 	return result;
 };
 
-AND.prototype.toString = function() {
+OR.prototype.toString = function() {
 	var s = "";
 	this.opnds.map(function(o) {
 		s += "|| " + o.toString();
